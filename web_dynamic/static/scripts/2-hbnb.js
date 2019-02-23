346b6adfbd4b080d8b722ea86d94ea35f222227c
JackWanaCode <kazucuong@yahoo.com>
2019-02-23 19:02:54 +0000

    fix the status return

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   /*check the aminity */
-  var addCheck = function() {
+  let addCheck = function() {
     let list = [];
     $('input[type=checkbox]').each(function () {
       if (this.checked) {
@@ -13,21 +13,10 @@ $(document).ready(function () {
   /* update status */
   let url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function(status) {
-    if (status === 'success') {
+    if (status.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
       $('#api_status').removeClass('available');
     }
   });
 });
-
-/*
-$(documen).ready(function () {
-  let ids = {};
-  $('input[type=checkbox]').click(function () {
-    if (this.checked) {
-      ids[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete ids[$(this).data('id')
-    }
-*/
