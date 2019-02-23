58403f5771494150613c1dce726f2880e395c53c
JackWanaCode <kazucuong@yahoo.com>
2019-02-23 18:36:53 +0000

    for add list

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,27 @@
+$(document).ready(function () {
+  /*check the aminity */
+  var addCheck = function() {
+    let list = [];
+    $('input[type=checkbox]').each(function () {
+      if (this.checked) {
+        list.push($(this).attr('data-name'));
+      }
+    });
+    $('div.amenities > h4').text(list.join(', '));
+  }
+  $('input[type=checkbox]').on('click', addCheck);
+  /* update status */
+  let url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function(status) {
+    if (status === 'success') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
+
+/*
 $(documen).ready(function () {
   let ids = {};
   $('input[type=checkbox]').click(function () {
@@ -6,4 +30,4 @@ $(documen).ready(function () {
     } else {
       delete ids[$(this).data('id')
     }
-    
+*/
