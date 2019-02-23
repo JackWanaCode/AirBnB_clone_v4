5db56df747ce38cf5cbe43bcac605dbe4bb1772c
JackWanaCode <kazucuong@yahoo.com>
2019-02-23 03:11:07 +0000

    first commit for amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  var addCheck = function() {
+    let list = [];
+    $('input[type=checkbox]').each(function () {
+      if (this.checked) {
+        list.push($(this).attr('data-name'));
+      }
+    });
+    $('div.amenities > h4').text(list.join(', '));
+  }
+  $('input[type=checkbox]').on('click', addCheck);
+});
