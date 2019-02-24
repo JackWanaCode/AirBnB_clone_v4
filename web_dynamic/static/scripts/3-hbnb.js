c64face389f9b0fbecc454149a4f3aa432159bae
JackWanaCode <kazucuong@yahoo.com>
2019-02-24 19:25:20 +0000

    just updated

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
         let d = data;
         $.get(user_url, function(data) {
           let user = data.first_name + ' ' + data.last_name;
-          $('section.places').append('<article><div class="title"><h2>' + d[i].name + '</h2><div class="price_by_night">' + d[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + d[i].max_guest + 'Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + d[i].number_rooms + 'Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + d[i].number_bathrooms + 'Bathroom</div></div><div class="user"><strong>Owner: ' + user + '</strong></div><div class="description">' + d[i].description + '</div></article>');
+          $('section.places').append('<article><div class="title"><h2>' + d[i].name + String(i) + '</h2><div class="price_by_night">' + d[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + d[i].max_guest + 'Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + d[i].number_rooms + 'Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + d[i].number_bathrooms + 'Bathroom</div></div><div class="user"><strong>Owner: ' + user + '</strong></div><div class="description">' + d[i].description + '</div></article>');
         });
       }
     },
