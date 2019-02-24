f69fe9c1bce68f7a91e3aa384395bc828f1e49b0
Hunt6666 <489@holbertonschool.com>
2019-02-24 15:07:49 -0800

    4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   /*check the aminity */
-  let list = [];
-  let id_list = []
+  let id_list = [];
   let addCheck = function() {
     let list = [];
     $('input[type=checkbox]').each(function () {
@@ -31,30 +30,23 @@ $(document).ready(function () {
       contentType: 'application/json',
       data: JSON.stringify(dict),
       success: function( data, textStatus, jQxhr ){
-        let count = 0;
-	let name_lst = [];
-	for (let j in data) {
-          name_lst[j] = data[j].name;
-	}
-	alert(name_lst);
-	name_lst.sort();
-	alert(name_lst);
-        let new_data = []
-	for (let j in name_lst) {
-          for (let k in data) {
-            if (data[k].name === name_lst[j]) {
-              new_data[j] = data[k];
-            }
-          }
-        }
-        for (let i in new_data) {
-          let user_url = 'http://0.0.0.0:5001/api/v1/users/' + new_data[i].user_id;
-          let d = new_data[i];
+      	data.sort(function (a, b) {
+          let keyA = a.name.toUpperCase();
+          let keyB = b.name.toUpperCase();
+          if(keyA < keyB) return -1;
+          if(keyA > keyB) return 1;
+          return 0;
+        });
+        for (let i in data) {
+          let d = data[i];
+          let user_url = 'http://0.0.0.0:5001/api/v1/users/' + d.user_id;
           $.get(user_url, function(data) {
             let user = data.first_name + ' ' + data.last_name;
-            $('section.places').append('<article><div class="title"><h2>' + d.name + '</h2><div class="price_by_night">' + d.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + d.max_guest + 'Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + d.number_rooms + 'Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + d.number_bathrooms + 'Bathroom</div></div><div class="user"><strong>Owner: ' + user + '</strong></div><div class="description">' + d.description + '</div></article>');
+            d.user_name = user;
           });
-          count++;
+        }
+        for (let i in data) {
+          $('section.places').append('<article><div class="title"><h2>' + data[i].name + '</h2><div class="price_by_night">' + data[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + data[i].max_guest + 'Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + data[i].number_rooms + 'Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + data[i].number_bathrooms + 'Bathroom</div></div><div class="user"><strong>Owner: ' + data[i].user + '</strong></div><div class="description">' + data[i].description + '</div></article>');
         }
       },
       error: function( jqXhr, textStatus, errorThrown ){
@@ -69,6 +61,6 @@ $(document).ready(function () {
     }
     $('article').remove();
     get_place(amens_dic);
-//    id_list = [];
+    id_list = []
   });
 });
