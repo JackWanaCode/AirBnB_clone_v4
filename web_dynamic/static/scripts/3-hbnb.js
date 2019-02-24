e30d31ac5328cb28ab1ff8cd464e3bb9f204688d
Hunt6666 <489@holbertonschool.com>
2019-02-24 11:44:28 -0800

    adding 3.js correction

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,4 +1,26 @@
-$.ajax({
+$(document).ready(function () {
+  /*check the aminity */
+  let addCheck = function() {
+    let list = [];
+    $('input[type=checkbox]').each(function () {
+      if (this.checked) {
+        list.push($(this).attr('data-name'));
+      }
+    });
+    $('div.amenities > h4').text(list.join(', '));
+  }
+  $('input[type=checkbox]').on('click', addCheck);
+  /* update status */
+  let url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function(status) {
+    if (status.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  /* search place handling */
+  $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     dataType: 'json',
     type: 'post',
@@ -7,10 +29,10 @@ $.ajax({
     success: function( data, textStatus, jQxhr ){
       for (let i in data) {
         let user_url = 'http://0.0.0.0:5001/api/v1/users/' + data[i].user_id;
-        let d = data;
+        let d = data[i];
         $.get(user_url, function(data) {
           let user = data.first_name + ' ' + data.last_name;
-          $('section.places').append('<article><div class="title"><h2>' + d[i].name + '</h2><div class="price_by_night">' + d[i].price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + d[i].max_guest + 'Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + d[i].number_rooms + 'Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + d[i].number_bathrooms + 'Bathroom</div></div><div class="user"><strong>Owner: ' + user + '</strong></div><div class="description">' + d[i].description + '</div></article>');
+          $('section.places').append('<article><div class="title"><h2>' + d.name + '</h2><div class="price_by_night">' + d.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + d.max_guest + 'Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + d.number_rooms + 'Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + d.number_bathrooms + 'Bathroom</div></div><div class="user"><strong>Owner: ' + user + '</strong></div><div class="description">' + d.description + '</div></article>');
         });
       }
     },
@@ -18,3 +40,4 @@ $.ajax({
         console.log( errorThrown );
     }
   });
+});
